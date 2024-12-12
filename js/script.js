64f6c73ea87a98f190a73bcdc00b5b62155d1949
Oksana Melnyk <69401769+comoartista@users.noreply.github.com>
2024-12-12 16:33:21 +0100

    Displayed popular movies

--- js/script.js
+++ js/script.js
@@ -5,6 +5,58 @@ const global = {
 const navLinkMovies = document.querySelector("#navLinkMovies");
 const navLinkShows = document.querySelector("#navLinkShows");
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  console.log(results);
+  results.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("card");
+    movieEl.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+    ${
+      movie.poster_path
+        ? `<img
+    src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+        : `<img
+          src="../images/no-image.jpg}"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+    }
+  </a>
+  <div class="card-body">
+    <h5 class="card-title">${movie.title}</h5>
+    <p class="card-text">
+      <small class="text-muted">Release: ${movie.release_date}
+     
+      </small>
+    </p>
+  </div>
+    `;
+
+    document.querySelector("#popular-movies").appendChild(movieEl);
+  });
+}
+//Fetch data from TMDB
+async function fetchAPIData(endpoint) {
+  const API_KEY = "8a2dbd267e35d1060b66f5634a4d97fe";
+  const API_URL = "https://api.themoviedb.org/3/"; // змінено URL
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+  return data;
+}
+
 //Highlight active link
 function highlightActiveLink() {
   const navLinks = document.querySelectorAll(".nav-link");
@@ -18,7 +70,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
