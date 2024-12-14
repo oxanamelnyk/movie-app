967192dafc5363663267ed48af695b0b34892462
Oksana Melnyk <69401769+comoartista@users.noreply.github.com>
2024-12-14 07:46:41 +0100

    movie details page

--- js/script.js
+++ js/script.js
@@ -74,6 +74,77 @@ async function displayPopularShows() {
     document.querySelector("#popular-shows").appendChild(showEl);
   });
 }
+
+//Display movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  console.log(movie);
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+      ? `<img
+  src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+      : `<img
+        src="../images/no-image.jpg}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"${movie.vote_average.toFixed(1)}></i>
+      8 / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}X</p>
+    <p>
+    ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+  ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+</ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addComasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addComasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+  ${movie.production_companies
+    .map((company) => ` <span">${company.name}</span>`)
+    .join(", ")}
+    </div>
+    </div>
+
+`;
+
+  document.querySelector("#movie-details").appendChild(div);
+}
 //Fetch data from TMDB
 async function fetchAPIData(endpoint) {
   const API_KEY = "8a2dbd267e35d1060b66f5634a4d97fe";
@@ -112,6 +183,10 @@ function highlightActiveLink() {
       link.classList.add("active");
   });
 }
+
+function addComasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 //Init App
 function init() {
   switch (global.currentPage) {
@@ -123,7 +198,7 @@ function init() {
       displayPopularShows();
       break;
     case "/movie-details.html":
-      console.log("Movie details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV details");
